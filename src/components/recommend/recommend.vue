12f8d9936f093e37e08a51df6126ed343c195ebe
郑磊 <zhenglei1@ifeng.com>
2017-07-03 20:02:41 +0800

    增加推荐页的api调用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -5,10 +5,24 @@
 </template>
 
 <script type="text/ecmascript-6">
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 export default {
   name: 'recommend',
   data () {
     return {
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
     }
   }
 }
