af9b5b33e801859a16882725083ab82473236d51
郑磊 <zhenglei1@ifeng.com>
2017-07-04 17:36:38 +0800

    增加推荐歌曲样式

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,16 +11,37 @@ var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
+var axios = require('axios')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
-// automatically open browser, if not set will be false
+  // automatically open browser, if not set will be false
 var autoOpenBrowser = !!config.dev.autoOpenBrowser
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
+  // Define HTTP proxies to your custom API backend
+  // https://github.com/chimurai/http-proxy-middleware
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+//路由
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function(req, res) {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+app.use('/api', apiRoutes)
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -29,21 +50,25 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {}
-})
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    log: () => {}
+  })
+  // force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function(compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+    hotMiddleware.publish({
+      action: 'reload'
+    })
     cb()
   })
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
+Object.keys(proxyTable).forEach(function(context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {
+      target: options
+    }
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
@@ -72,7 +97,7 @@ var readyPromise = new Promise(resolve => {
 console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
-  // when env is testing, don't need open it
+    // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
   }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,29 +1,67 @@
 <template>
-  <div class="recommend">
-     推荐页面
+  <div class="recommend" ref="recommend">
+     <div class="recommend-content">
+       <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+         <slider>
+           <div v-for="item in recommends">
+             <a :href="item.linkUrl">
+               <img :src="item.picUrl"/>
+             </a>
+           </div>
+          </slider>
+       </div>
+       <div class="recommend-list">
+         <h1 class="list-title">热门歌单推荐</h1>
+         <ul>
+           <li class="item" v-for="item in discList">
+             <div class="icon">
+                <img width="60" height="60" :src="item.imgurl"/>
+             </div>
+             <div class="text">
+               <h2 class="name">{{item.creator.name}}</h2>
+               <p class="desc">{{item.dissname}}</p>
+             </div>
+           </li>
+         </ul>
+       </div>
+     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { getRecommend } from 'api/recommend'
+import Slider from 'base/slider/slider'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 export default {
   name: 'recommend',
   data () {
     return {
+      recommends: [],
+      discList: []
     }
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
+          this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
@@ -31,6 +69,49 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-  @import "~common/stylus/mixin"
+
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            font-size: $font-size-medium
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            text-align: left
+            line-height: 20px
+            overflow: hidden
+            .name
+              margin-bottom: 5px
+              color: $color-text
+            .desc
+              color: $color-text-d
+
 
 </style>
