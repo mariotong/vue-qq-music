2c7b76ed9a4f02bae0b1a93eafa91d1c3295bf66
郑磊 <zhenglei1@ifeng.com>
2017-07-16 20:47:10 +0800

    歌曲播放组件可以播放歌曲

---
+++ src/base/progress-circle/progress-circle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="progress-circle">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'progressCircle',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .progress-circle
+    position: relative
+</style>
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,35 +1,104 @@
 <template>
-  <div class="player">
-     <div class="normal-player">
-       <div class="background">
-         <img width="100%" height="100%" :src="currentSong.image"/>
-       </div>
-       <div class="top">
-         <div class="back">
-           <i class="icon-back"></i>
+  <div class="player" v-show="playlist.length > 0">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
+      <div class="normal-player" v-show="fullScreen">
+         <div class="background">
+           <img width="100%" height="100%" :src="currentSong.image"/>
          </div>
-         <h1 class="title" v-html="currentSong.name"></h1>
-         <h2 class="subtitle" v-html="currentSong.singer"></h2>
-       </div>
-       <div class="middle">
-         <div class="middle-l">
-           <div class="cd-wrapper">
-             <div class="cd">
-               <img class="image" :src="currentSong.image">
+         <div class="top">
+           <div class="back" @click="back">
+             <i class="icon-back"></i>
+           </div>
+           <h1 class="title" v-html="currentSong.name"></h1>
+           <h2 class="subtitle" v-html="currentSong.singer"></h2>
+         </div>
+         <div class="middle">
+           <div class="middle-l">
+             <div class="cd-wrapper" ref="cdWrapper">
+               <div class="cd">
+                 <img class="image" :src="currentSong.image">
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="bottom">
+           <div class="dot-wrapper">
+             <span class="dot"></span>
+             <span class="dot"></span>
+           </div>
+           <div class="progress-wrapper">
+             <span class="time time-l">{{format(currentTime)}}</span>
+             <div class="progress-bar-wrapper">
+               <progress-bar></progress-bar>
+             </div>
+             <span class="time time-r">{{format(currentSong.duration)}}</span>
+           </div>
+           <div class="operators">
+             <div class="icon i-left">
+               <i class="icon-sequence"></i>
+             </div>
+             <div class="icon i-left">
+               <i class="icon-prev"></i>
+             </div>
+             <div class="icon i-center">
+               <i @click="togglePlaying" class="icon-pause"></i>
+             </div>
+             <div class="icon i-right">
+               <i class="icon-next"></i>
+             </div>
+             <div class="icon i-right">
+               <i class="icon icon-favorite""></i>
              </div>
            </div>
          </div>
        </div>
-     </div>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img class="pause" width="40" height="40" :src="currentSong.image"/>
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control">
+          <!-- <progress-circle>
+            <i class="icon-mini icon-pause-mini"></i>
+          </progress-circle> -->
+          <i class="icon-mini icon-pause-mini"></i>
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
+        </div>
+      </div>
+    </transition>
+    <audio ref="audio" :src="currentSong.url" @play="ready" @error="error"
+           @ended="end" @timeupdate="updateTime"></audio>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
+import ProgressBar from 'base/progress-bar/progress-bar'
+import ProgressCircle from 'base/progress-circle/progress-circle'
+import animations from 'create-keyframe-animation'
+import {prefixStyle} from 'common/js/dom'
+
+const transform = prefixStyle('transform')
+// const transitionDuration = prefixStyle('transitionDuration')
+
 export default {
   name: 'player',
   data () {
     return {
+      currentTime: 0,
+      songReady: false
     }
   },
   computed: {
@@ -37,8 +106,123 @@ export default {
       'currentIndex',
       'fullScreen',
       'playing',
+      'playlist',
       'currentSong'
     ])
+  },
+  methods: {
+    back() {
+      this.setFullScreen(false)
+    },
+    open() {
+      this.setFullScreen(true)
+    },
+    enter(el, done) {
+      const {x, y, scale} = this._getPosAndScale()
+
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0, 0, 0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0, 0, 0) scale(1)`
+        }
+      }
+
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter() {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave(el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave() {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPosAndScale() {
+      const targetWidth = 40
+      const paddingLeft = 40
+      const paddingBottom = 30
+      const paddingTop = 80
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+      // 那个中间的CD要移动的距离，x是负值， y是正值，x向左移，y向下移
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return {
+        x,
+        y,
+        scale
+      }
+    },
+    ready() {
+      this.songReady = true
+    },
+    error() {
+      this.songReady = true
+    },
+    end() {
+
+    },
+    updateTime(e) {
+      this.currentTime = e.target.currentTime
+    },
+    format(interval) {
+      interval = interval | 0
+      const minute = interval / 60 | 0
+      const second = this._pad(interval % 60)
+      return `${minute}:${second}`
+    },
+    _pad(num, n = 2) {
+      let len = num.toString().length
+      while (len < n) {
+        num = '0' + num
+        len++
+      }
+      return num
+    },
+    togglePlaying() {
+
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN'
+    })
+  },
+  watch: {
+    currentSong(newSong, oldSong) {
+      if (!newSong.id) {
+        return
+      }
+      if (newSong.id === oldSong.id) {
+        return
+      }
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.$refs.audio.play()
+      }, 1000)
+    }
+  },
+  components: {
+    ProgressBar,
+    ProgressCircle
   }
 }
 </script>
@@ -126,4 +310,112 @@ export default {
                 width: 100%
                 height: 100%
                 border-radius: 50%
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .dot-wrapper
+          text-align: center
+          font-size: 0
+          .dot
+            display: inline-block
+            vertical-align: middle
+            margin: 0 4px
+            width: 8px
+            height: 8px
+            border-radius: 50%
+            background: $color-text-ll
+        .progress-wrapper
+          display: flex
+          align-items: center
+          width: 80%
+          margin: 0 auto
+          padding: 10px 0
+          .time
+            color: $color-text
+            font-size: $font-size-small
+            flex: 0 0 30px
+            &.time-l
+              text-align: left
+            &.time-r
+              text-align: right
+          .progress-bar-wrapper
+            flex: 1
+        .operators
+          display: flex
+          align-items: center
+          .icon
+            flex: 1
+            color: $color-theme
+            i
+              font-size: 30px
+          .i-left
+            text-align: right
+          .i-center
+            padding: 0 20px
+            text-align: center
+            i
+              font-size: 40px
+          .i-right
+            text-align: left
+          .icon-favorite
+            color: $color-sub-theme
+      &.normal-enter-active, &.normal-leave-active
+        transition: all 0.4s
+        .top, .bottom
+          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+      &.normal-enter, &.normal-leave-to
+        opacity: 0
+        .top
+          transform: translate3d(0, -100px, 0)
+        .bottom
+          transform: translate3d(0, 100px, 0)
+    .mini-player
+      display: flex
+      align-items: center
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background: $color-highlight-background
+      &.mini-enter-active, &.mini-leave-active
+        transition: all 0.4s
+      &.mini-enter, &.mini-leave-to
+        opacity: 0
+      .icon
+        flex: 0 0 40px
+        width: 40px
+        padding: 0 10px 0 20px
+        img
+          border-radius: 50%
+      .text
+        display: flex
+        flex: 1
+        flex-direction: column
+        justify-content: center
+        line-height: 20px
+        overflow: hidden
+        .name
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d
+      .control
+        flex: 0 0 30px
+        width: 30px
+        padding: 0 10px
+        .icon-play-mini, .icon-pause-mini, .icon-playlist
+          font-size: 30px
+          color: $color-theme-d
+        /*.icon-mini
+          font-size: 32px
+          position: absolute
+          left: 0
+          top: 0*/
 </style>
